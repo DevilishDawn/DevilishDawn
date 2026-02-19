d2d7d2d192d2e249d771e06ad0fc48123d775860
Dawn <DevilishDawn43@proton.me>
2026-02-19 11:19:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Mainpage Script
+
+
+var button1 = document.getElementByClass("gitbtn");
+
+button.onmouseover = function() {
+  document.getElementByClass("title") = "Github";
+}
+
+button.onmouseout = function() {
+  document.getElementByClass("title") = "Dawn's Cozy Webpage";
+}
